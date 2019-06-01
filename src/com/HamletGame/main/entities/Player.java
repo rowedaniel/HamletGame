02e90079e99604859db86a20ab67ddc04b7b95d3
Daniel Rowe <rowedanielp@gmail.com>
2019-05-31 19:18:28 -0700

    fixed janky player code

--- src/com/HamletGame/main/entities/Player.java
+++ src/com/HamletGame/main/entities/Player.java
@@ -9,12 +9,12 @@ import com.HamletGame.main.graphics.ImageTile;
 public class Player extends GameObject{
 
 	private boolean LEFT, RIGHT, UP, DOWN;
-	private long walkSpeed = (long)5000000; // px/s
+	private double walkSpeed = 0.2f; // px/s
 	private ImageTile image;
 	private int animationX;
 	private int animationY;
 	private double animationTime = 0.0f;
-	private long animationSpeed = (long)5000000 * 10; // 1/10 fps
+	private double animationSpeed = 0.05f; // 1/10 fps
 	private double animationFactor = 1.0f;
 	
 	public Player(int x, int y, ID id) {
@@ -26,10 +26,10 @@ public class Player extends GameObject{
 	
 	@Override
 	public void update(long delta) {
-		x += xvel*(delta/walkSpeed); // 10**-9 * xvel px/s
-		y += yvel*(delta/walkSpeed);
-		extraX += xvel*(delta/(double)walkSpeed) - xvel*delta/walkSpeed;
-		extraY += yvel*(delta/(double)walkSpeed) - yvel*delta/walkSpeed;
+		x += xvel*(delta/1000000000*walkSpeed); // 10**-9 * xvel px/s
+		y += yvel*(delta/1000000000/walkSpeed);
+		extraX += xvel*(delta/1000000000.0*walkSpeed) - xvel*delta/1000000000*walkSpeed;
+		extraY += yvel*(delta/1000000000.0*walkSpeed) - yvel*delta/1000000000*walkSpeed;
 		x += (int)extraX;
 		y += (int)extraY;
 		extraX -= (int)extraX;
@@ -42,42 +42,42 @@ public class Player extends GameObject{
 		
 		// update animation
 		animationX = (int)animationTime;
-		animationTime += ((double)delta*animationFactor/animationSpeed);
+		animationTime += ((double)delta*animationFactor*animationSpeed/1000000000.0);
 		animationTime %= 8;
 		
-		 if(LEFT) {
-			 setAnimationState(2);
-			 setVelX(-1);
-		 } else if(RIGHT) {
-			 setAnimationState(4);
-			 setVelX(1);
-		 } else {
-			 setVelX(0);
-		 }
-		 if(UP) {
-			 setAnimationState(1);
-			 setVelY(-1);
-		 } else if(DOWN) {
-			 setAnimationState(3);
-			 setVelY(1);
-		 } else {
-			 setVelY(0);
-		 }
-       if(RIGHT && DOWN) {
-          setAnimationState(5);
-       }
-       if(LEFT && DOWN) {
-          setAnimationState(6);
-       }
-       if(RIGHT && UP) {
-          setAnimationState(7);
-       }
-       if(LEFT && UP) {
-          setAnimationState(8);
-       }
-		 if(!(LEFT || RIGHT || UP || DOWN)) {
-			 setAnimationState(0);
-		 }
+		if(LEFT) {
+			setAnimationState(2);
+			setVelX(-1);
+		} else if(RIGHT) {
+			setAnimationState(4);
+			setVelX(1);
+		} else {
+			setVelX(0);
+		}
+		if(UP) {
+			setAnimationState(1);
+			setVelY(-1);
+		} else if(DOWN) {
+			setAnimationState(3);
+			setVelY(1);
+		} else {
+			setVelY(0);
+		}
+		if(RIGHT && DOWN) {
+			setAnimationState(5);
+        }
+        if(LEFT && DOWN) {
+        	setAnimationState(6);
+        }
+        if(RIGHT && UP) {
+        	setAnimationState(7);
+        }
+        if(LEFT && UP) {
+        	setAnimationState(8);
+        }
+		if(!(LEFT || RIGHT || UP || DOWN)) {
+			setAnimationState(0);
+		}
 	}
 	
 	public void setAnimationState(int state) {
